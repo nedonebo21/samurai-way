8fd2940a0864d88105523523c29d05315c567aab
nedonebo21 <cursed.void10007@gmail.com>
2025-04-30 23:12:09 +0400

    create NavBar component & css module for NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,21 +1,22 @@
 import React from "react";
+import s from './NavBar.module.css'
 
 export const NavBar = () => {
     return (
-        <nav className="nav">
-            <div>
+        <nav className={s.nav}>
+            <div className={`${s.item} ${s.active}`}>
                 <a href="#">Profile</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a href="#">Messages</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a href="#">News</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a href="#">Music</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a href="#">Settings</a>
             </div>
         </nav>
