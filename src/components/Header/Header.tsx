140940b507f3d064d396c68167853f9be1be3d0e
nedonebo21 <cursed.void10007@gmail.com>
2025-04-30 23:10:59 +0400

    create Header component & css module for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Header.module.css"
 
 export const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img
                 src="https://i.redd.it/free-svg-this-svg-from-anime-called-naruto-turn-it-from-low-v0-eoig0s8a5sh91.png?width=3500&format=png&auto=webp&s=1006757cc5fff6561f72581bf021962c4402b16d"
                 alt=""/>
