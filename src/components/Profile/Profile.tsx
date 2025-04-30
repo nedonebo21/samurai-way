8d1a3035e9e351e14da4295ca862d08e9b80841b
nedonebo21 <cursed.void10007@gmail.com>
2025-04-30 23:11:52 +0400

    create Profile component & css module for Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Profile.module.css"
 
 export const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img
                     src="https://i.ytimg.com/vi/yLRaDfPSB-4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBJJjQdF_u5FifTIMKMDhilm3S0hA"
@@ -16,10 +17,10 @@ export const Profile = () => {
                 <div>
                     New Post
                 </div>
-                <div>
-                    <div>Post1</div>
-                    <div>Post2</div>
-                    <div>Post3</div>
+                <div className={s.posts}>
+                    <div className={s.item}>Post1</div>
+                    <div className={s.item}>Post2</div>
+                    <div className={s.item}>Post3</div>
                 </div>
             </div>
         </div>
