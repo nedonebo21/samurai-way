de1072c244e04b4fde12012810d586c86f99b82e
nedonebo21 <cursed.void10007@gmail.com>
2025-05-02 11:46:12 +0400

    add styles & use Avatar component(15)

--- src/components/Profile/Description/Description.tsx
+++ src/components/Profile/Description/Description.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import s from './Description.module.css'
+import {Avatar} from "../Avatar/Avatar";
 
 export const Description = () => {
     return (
         <div className={s.description}>
-            <img
-                src="https://i.ytimg.com/vi/qbxteW4kI6k/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGHEgOyhyMA8=&rs=AOn4CLDiLrIP6bTF4-NrmKQH970OwjNRsg"
-                alt=""/>
+            <Avatar/>
             <div className={s.info}>
                 <strong>Info</strong>
                 <div><strong>Name:</strong> Stanislav</div>
