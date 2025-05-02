f9768e6513e23a7a874cc0639e9f92b0c2895e21
nedonebo21 <cursed.void10007@gmail.com>
2025-05-02 17:15:27 +0400

    create Dialogs page component + styles in body(18)

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 
 export const App = () => {
     return (
         <div className="app-wrapper">
             <Header/>
             <NavBar/>
-            <Profile/>
+            <div className="app-wrapper-content">
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
         </div>
     )
 }
