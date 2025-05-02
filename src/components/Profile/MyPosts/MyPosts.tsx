60a17dc7a0365c24681acf5317c323713f02ae68
nedonebo21 <cursed.void10007@gmail.com>
2025-05-02 11:45:42 +0400

    add styles & add posts with component(15)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import s from "./MyPosts.module.css";
+import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
     return (
-        <div>
+        <div className={s.posts_wrapper}>
             my posts
             <div>
-                New Post
+                <textarea ></textarea>
+                <button>Add Post</button>
+                <button>Remove</button>
             </div>
             <div className={s.posts}>
-                <div className={s.item}>Post1</div>
-                <div className={s.item}>Post2</div>
-                <div className={s.item}>Post3</div>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
             </div>
         </div>
     );
