a7d35e96285c7812d2ade91a121c5728819dd521
nedonebo21 <cursed.void10007@gmail.com>
2025-05-02 11:43:27 +0400

    create Avatar component(15)

---
+++ src/components/Profile/Avatar/Avatar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Avatar.module.css';
+
+export const Avatar = () => {
+    return (
+        <img className={s.img}
+            src="https://i.ytimg.com/vi/qbxteW4kI6k/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGHEgOyhyMA8=&rs=AOn4CLDiLrIP6bTF4-NrmKQH970OwjNRsg"
+            alt=""/>
+    );
+};
