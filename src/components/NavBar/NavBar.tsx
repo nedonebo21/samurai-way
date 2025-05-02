b8efcf28622ed5f0d3661208738d80082a9afd9f
nedonebo21 <cursed.void10007@gmail.com>
2025-05-02 20:51:24 +0400

    add activeClassName + styles(20)

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,19 +6,19 @@ export const NavBar = () => {
     return (
         <nav className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/profile">Profile</NavLink>
+                <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/dialogs">Dialogs</NavLink>
+                <NavLink to="/dialogs" activeClassName={s.active}>Dialogs</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/news">News</NavLink>
+                <NavLink to="/news" activeClassName={s.active}>News</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/music">Music</NavLink>
+                <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/settings">Settings</NavLink>
+                <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
             </div>
         </nav>
     )
