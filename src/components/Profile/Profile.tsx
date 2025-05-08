c12d4d0069d446c90ffe782df98974a71fe2b0b6
nedonebo21 <cursed.void10007@gmail.com>
2025-05-08 14:40:50 +0400

    create ProfileInfo component + move styles(23)

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import s from "./Profile.module.css"
-import {Description} from "./Description/Description";
 import {MyPosts} from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
+
+
 
 export const Profile = () => {
     return (
         <div>
-            <div className={s.bg}>
-                <img
-                    src="https://i.ytimg.com/vi/yLRaDfPSB-4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBJJjQdF_u5FifTIMKMDhilm3S0hA"
-                    alt=""/>
-            </div>
-            <Description/>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,16 @@
+import s from "./ProfileInfo.module.css";
+import {Description} from "../Description/Description";
+import React from "react";
+
+export const ProfileInfo = () => {
+    return (
+        <>
+            <div className={s.bg}>
+                <img
+                    src="https://i.ytimg.com/vi/yLRaDfPSB-4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBJJjQdF_u5FifTIMKMDhilm3S0hA"
+                    alt=""/>
+            </div>
+            <Description/>
+        </>
+    )
+}
