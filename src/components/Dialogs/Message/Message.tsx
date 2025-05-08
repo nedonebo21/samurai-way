ff970f9afd05a2f3f78e634bb5323649df575adb
nedonebo21 <cursed.void10007@gmail.com>
2025-05-08 17:57:57 +0400

    move Message component to individual file + styles(25)

---
+++ src/components/Dialogs/Message/Message.tsx
@@ -0,0 +1,11 @@
+import s from "./Message.module.css";
+import React from "react";
+
+type Props = {
+    message: string
+}
+export const Message = ({message}: Props) => {
+    return (
+        <div className={s.message}>{message}</div>
+    )
+}
