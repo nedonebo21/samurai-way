5a98862ffee4448d99b886feaf3a7e82bc77920d
nedonebo21 <cursed.void10007@gmail.com>
2025-05-08 15:07:05 +0400

    switch img to bg-img + styles

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -6,9 +6,9 @@ export const ProfileInfo = () => {
     return (
         <>
             <div className={s.bg}>
-                <img
-                    src="https://i.ytimg.com/vi/yLRaDfPSB-4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBJJjQdF_u5FifTIMKMDhilm3S0hA"
-                    alt=""/>
+                {/*<img*/}
+                {/*    src="https://i.ytimg.com/vi/yLRaDfPSB-4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBJJjQdF_u5FifTIMKMDhilm3S0hA"*/}
+                {/*    alt=""/>*/}
             </div>
             <Description/>
         </>
