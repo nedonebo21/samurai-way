48c38097cde46398c16d295be16aad3848c22b88
nedonebo21 <cursed.void10007@gmail.com>
2025-05-08 13:27:26 +0400

    add styles, structure in Dialogs page(21)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,16 @@ import s from './Dialogs.module.css'
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
-            Dialogs
+            <div className={s.dialogs_items}>
+                <div className={s.dialog + ' ' + s.active}>4el 1</div>
+                <div className={s.dialog}>4el 2</div>
+                <div className={s.dialog}>4el 3</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>Its really your social network</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
