67211c245d4ab18351ef97b783923bef3d963cec
nedonebo21 <cursed.void10007@gmail.com>
2025-05-09 17:07:46 +0400

    create imgUrl in state.tsx(29)

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,6 +1,7 @@
 type UserDataType = {
     id: string
     name: string
+    imgUrl: string
 }
 type MessageType = {
     id: string
@@ -39,13 +40,13 @@ export const state: StateType = {
     },
     dialogsPage: {
         usersDialogsData: [
-            {id: "1", name: "Pudge"},
-            {id: "2", name: "Morphling"},
-            {id: "3", name: "KOTL"},
-            {id: "4", name: "Viper"},
-            {id: "5", name: "Phoenix"},
-            {id: "6", name: "Ember"},
-            {id: "7", name: "Storm"},
+            {id: "1", name: "Pudge", imgUrl: "https://play-lh.googleusercontent.com/82HFnMT2VbR8wgl6_a17UppNiNvzmyafK0BJW4FW4h-CV4BZq2dGTisboxOYNYI5gLDe=w240-h480-rw"},
+            {id: "2", name: "Morphling", imgUrl: "https://dota2.ru/img/heroes/morphling/portrait.jpg"},
+            {id: "3", name: "KOTL", imgUrl: "https://preview.redd.it/ygt394pc4uz81.jpg?width=300&format=pjpg&auto=webp&s=931c83697822508002307f79ebb05da97537afa4"},
+            {id: "4", name: "Viper", imgUrl: "https://dota2.ru/img/heroes/viper/portrait.jpg"},
+            {id: "5", name: "Phoenix", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLL8ZVV8mtzPPZ2mR_FtYyok4o2t36qMLeWtL3Cz-r9lRKZkgukBZsQtTH2p7i-9SKhtk&usqp=CAU"},
+            {id: "6", name: "Ember", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNG9-yTA20s2dFLEJamV7CilxHAIkU3Oc6HA&s"},
+            {id: "7", name: "Storm", imgUrl: "https://i.ytimg.com/vi/X1oXuyG6YxI/maxresdefault.jpg"},
         ],
         messagesData: [
             {id: "1", message: "Yo"},
