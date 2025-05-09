bcf4912a1c1b3082d9e1cfd059db6f105256776a
nedonebo21 <cursed.void10007@gmail.com>
2025-05-09 18:06:30 +0400

    render online users .map + styles(29)

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import s from './NavBar.module.css'
 import {NavLink} from "react-router-dom";
+import {SideBarType} from "../../redux/state";
 
-export const NavBar = () => {
+
+export const NavBar = ({usersOnlineData}: SideBarType) => {
+
+    const usersOnlineItems = usersOnlineData.map((user) => (
+        user.isOnline ?
+            <li className={s.friends_item} key={user.id}>
+                <img src={user.imgUrl} alt=""/>
+                {user.name}
+                <div className={s.online_circle}></div>
+            </li> : null
+    ))
+
     return (
         <nav className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
@@ -19,6 +31,13 @@ export const NavBar = () => {
             </div>
             <div className={s.item}>
                 <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+            </div>
+
+            <div className={s.friends_online}>
+                <h3>Friends Online</h3>
+                <ul className={s.friends_items}>
+                    {usersOnlineItems}
+                </ul>
             </div>
         </nav>
     )
