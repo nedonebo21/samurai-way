320d47a27e940a7b3b3455cbf9343a00ccefd466
nedonebo21 <cursed.void10007@gmail.com>
2025-05-15 15:06:22 +0400

    add Ref,onClick + send message on press "enter"(31)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {KeyboardEvent} from 'react';
 import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
@@ -19,6 +19,14 @@ export const Dialogs = ({messagesData, usersDialogsData}: MessagesPageType) => {
         : <p>No dialogs, you should find friends :)</p>
     const messagesItems = messagesData.length ? messagesList
         : <p>No messages. Write smth to start dialog</p>
+    const newMessage = React.createRef<any>()
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === 'Enter') sendMessage()
+    }
+    const sendMessage = () => {
+        const message = newMessage.current.value
+        alert(message)
+    }
 
     return (
         <div className={s.dialogs}>
@@ -28,8 +36,8 @@ export const Dialogs = ({messagesData, usersDialogsData}: MessagesPageType) => {
             <div className={s.messages}>
                 {messagesItems}
                 <div className={s.send_message}>
-                    <textarea placeholder={"Пишем"}></textarea>
-                    <button> →</button>
+                    <textarea onKeyDown={onKeyDownHandler} ref={newMessage} placeholder={"Пишем"}></textarea>
+                    <button onClick={sendMessage}> →</button>
                 </div>
             </div>
         </div>
