d0ce39a7671afa3626cebfbce3ccefe49c2a8ff3
nedonebo21 <cursed.void10007@gmail.com>
2025-06-28 17:04:17 +0400

    add follow/unfollow api (62)

--- src/components/users/users-container.tsx
+++ src/components/users/users-container.tsx
@@ -31,7 +31,9 @@ type UsersApiType = {
 class UsersApiComponent extends React.Component<UsersApiType> {
     componentDidMount() {
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
+            withCredentials: true,
+        })
             .then(res => {
                 this.props.toggleIsFetching(false)
                 this.props.setUsers(res.data.items)
@@ -41,7 +43,9 @@ class UsersApiComponent extends React.Component<UsersApiType> {
     onPageChanged = (pageNumber: number) => {
         this.props.setCurrentPage(pageNumber)
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+            withCredentials: true,
+        })
             .then(res => {
                 this.props.toggleIsFetching(false)
                 this.props.setUsers(res.data.items)
--- src/components/users/users.tsx
+++ src/components/users/users.tsx
@@ -5,70 +5,89 @@ import {UserIcon} from "../../shared/ui/user-icon/user-icon";
 import s from './users.module.css'
 import {User} from "../../redux/types/state-types";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 type UsersType = {
-    totalUsersCount: number
-    pageSize: number
-    currentPage: number
-    onPageChanged: (pageNumber: number) => void
-    follow: (userId: number) => void
-    unFollow: (userId: number) => void
-    users: User[]
+  totalUsersCount: number
+  pageSize: number
+  currentPage: number
+  onPageChanged: (pageNumber: number) => void
+  follow: (userId: number) => void
+  unFollow: (userId: number) => void
+  users: User[]
 }
 export const Users = (props: UsersType) => {
 
-    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
-    let pages = []
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
+  const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+  let pages = []
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
+  }
 
-    return (
-        <div className={s.user_items}>
-            {
-                props.users.map(el => {
-                    return (
-                        <div className={s.user_item} key={el.id}>
-                            <div className={s.user_action}>
-                                <div>
-                                    <NavLink to={`/profile/${el.id}`}>
-                                        <UserIcon avatarUrl={el.photos.small !== null ? el.photos.small : defaultAvatar}/>
-                                    </NavLink>
-                                </div>
-                                <div>{el.followed
-                                    ? <Button onClick={() => {
-                                        props.unFollow(el.id)
-                                    }}>Unfollow</Button>
-                                    : <Button onClick={() => {
-                                        props.follow(el.id)
-                                    }}>Follow</Button>
-                                }
-                                </div>
-                            </div>
-                            <div className={s.user_description}>
-                                <div className={s.user_info}>
-                                    <div className={s.user_name}>{el.name}</div>
-                                    <div className={s.user_status}>{el.status}</div>
-                                </div>
-                                <div className={s.user_location}>
-                                    <div className={s.user_country}>{'el.location.country'}</div>
-                                    <div className={s.user_city}>{'el.location.city'}</div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-            <div className={s.btn_block}>
-                {pages.map(el => {
-                    return <Button
-                        disabled={props.currentPage === el}
-                        className={props.currentPage === el ? s.selected_page : ''}
-                        onClick={() => {
-                            props.onPageChanged(el)
-                        }}>{el}</Button>
-                })}
-            </div>
+  return (
+      <div className={s.user_items}>
+        {
+          props.users.map(el => {
+            return (
+                <div className={s.user_item} key={el.id}>
+                  <div className={s.user_action}>
+                    <div>
+                      <NavLink to={`/profile/${el.id}`}>
+                        <UserIcon avatarUrl={el.photos.small !== null ? el.photos.small : defaultAvatar}/>
+                      </NavLink>
+                    </div>
+                    <div>{el.followed
+                        ? <Button onClick={() => {
+                          axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${el.id}`, {
+                            withCredentials: true,
+                            headers: {
+                              'API-KEY': '0eb062ed-280d-4c95-bc85-37aebbf14498'
+                            }
+                          }).then(res => {
+                            if (res.data.resultCode === 0) {
+                              props.unFollow(el.id)
+                            }
+                          })
+                        }}>Unfollow</Button>
+                        : <Button onClick={() => {
+                          axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${el.id}`, {}, {
+                            withCredentials: true,
+                            headers: {
+                              'API-KEY': '0eb062ed-280d-4c95-bc85-37aebbf14498'
+                            }
+                          }).then(res => {
+                            if (res.data.resultCode === 0) {
+                              props.follow(el.id)
+                            }
+                          })
+                        }}>Follow</Button>
+                    }
+                    </div>
+                  </div>
+                  <div className={s.user_description}>
+                    <div className={s.user_info}>
+                      <div className={s.user_name}>{el.name}</div>
+                      <div className={s.user_status}>{el.status}</div>
+                    </div>
+                    <div className={s.user_location}>
+                      <div className={s.user_country}>{'el.location.country'}</div>
+                      <div className={s.user_city}>{'el.location.city'}</div>
+                    </div>
+                  </div>
+                </div>
+            )
+          })
+        }
+        <div className={s.btn_block}>
+          {pages.map(el => {
+            return <Button
+                disabled={props.currentPage === el}
+                className={props.currentPage === el ? s.selected_page : ''}
+                onClick={() => {
+                  props.onPageChanged(el)
+                }}>{el}</Button>
+          })}
         </div>
-    )
+      </div>
+  )
 }
