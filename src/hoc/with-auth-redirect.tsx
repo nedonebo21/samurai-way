144b3f475252a998155111da177b400289faf885
nedonebo21 <cursed.void10007@gmail.com>
2025-07-12 21:44:51 +0400

    refactor hoc component & add types(70)

--- src/hoc/with-auth-redirect.tsx
+++ src/hoc/with-auth-redirect.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import {Redirect} from "react-router-dom";
+import {StateType} from "../redux/types/state-types";
+import {connect} from "react-redux";
 
 interface AuthProps {
   isAuth: boolean
 }
 
+let mapStateToPropsForRedirect = (state: StateType) => ({
+  isAuth: state.auth.isAuth,
+})
+
 export const WithAuthRedirect = <T extends AuthProps>(Component: React.ComponentType<T>) => {
-  class RedirectComponent extends React.Component<T>{
+  class RedirectComponent extends React.Component<AuthProps>{
     render(){
       if (!this.props.isAuth) return <Redirect to={'/login'}/>
-      return <Component {...this.props}/>
+      return <Component {...this.props as T}/>
     }
   }
-  return RedirectComponent
+
+  //TODO придумать чета с any (потом)
+  return connect(mapStateToPropsForRedirect)(RedirectComponent as any)
 }
