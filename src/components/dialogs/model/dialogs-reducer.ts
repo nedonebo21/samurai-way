974995dff0261a43e5941c77e2ecdeb0c5ed1308
nedonebo21 <cursed.void10007@gmail.com>
2025-07-27 13:52:09 +0400

    refactor reducers(76)

--- src/components/dialogs/model/dialogs-reducer.ts
+++ src/components/dialogs/model/dialogs-reducer.ts
@@ -1,60 +1,52 @@
 import {MessagesPageType, MessageType} from "../../../shared/types/state-types";
-import {ActionsType, AddMessageActionType, UpdateMessageTextType} from "../../../shared/types/action-types";
+import {ActionsType, AddMessageActionType} from "../../../shared/types/action-types";
 
 let initialState = {
-    usersDialogsData: [
-        {
-            id: "1",
-            name: "Pudge",
-            imgUrl: "https://play-lh.googleusercontent.com/82HFnMT2VbR8wgl6_a17UppNiNvzmyafK0BJW4FW4h-CV4BZq2dGTisboxOYNYI5gLDe=w240-h480-rw"
-        },
-        {id: "2", name: "Morphling", imgUrl: "https://dota2.ru/img/heroes/morphling/portrait.jpg"},
-        {
-            id: "3",
-            name: "KOTL",
-            imgUrl: "https://preview.redd.it/ygt394pc4uz81.jpg?width=300&format=pjpg&auto=webp&s=931c83697822508002307f79ebb05da97537afa4"
-        },
-        {id: "4", name: "Viper", imgUrl: "https://dota2.ru/img/heroes/viper/portrait.jpg"},
-        {
-            id: "5",
-            name: "Phoenix",
-            imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLL8ZVV8mtzPPZ2mR_FtYyok4o2t36qMLeWtL3Cz-r9lRKZkgukBZsQtTH2p7i-9SKhtk&usqp=CAU"
-        },
-        {
-            id: "6",
-            name: "Ember",
-            imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNG9-yTA20s2dFLEJamV7CilxHAIkU3Oc6HA&s"
-        },
-        {id: "7", name: "Storm", imgUrl: "https://i.ytimg.com/vi/X1oXuyG6YxI/maxresdefault.jpg"},
-    ],
-    messagesData: [
-        {id: "1", message: "Yo"},
-        {id: "2", message: "Its really your social network??"},
-        {id: "3", message: "Do you like gachi cinema??"},
-        {id: "4", message: "Yes."},
-    ],
-    newMessageText: 'asd'
+  usersDialogsData: [
+    {
+      id: "1",
+      name: "Pudge",
+      imgUrl: "https://play-lh.googleusercontent.com/82HFnMT2VbR8wgl6_a17UppNiNvzmyafK0BJW4FW4h-CV4BZq2dGTisboxOYNYI5gLDe=w240-h480-rw"
+    },
+    {id: "2", name: "Morphling", imgUrl: "https://dota2.ru/img/heroes/morphling/portrait.jpg"},
+    {
+      id: "3",
+      name: "KOTL",
+      imgUrl: "https://preview.redd.it/ygt394pc4uz81.jpg?width=300&format=pjpg&auto=webp&s=931c83697822508002307f79ebb05da97537afa4"
+    },
+    {id: "4", name: "Viper", imgUrl: "https://dota2.ru/img/heroes/viper/portrait.jpg"},
+    {
+      id: "5",
+      name: "Phoenix",
+      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLL8ZVV8mtzPPZ2mR_FtYyok4o2t36qMLeWtL3Cz-r9lRKZkgukBZsQtTH2p7i-9SKhtk&usqp=CAU"
+    },
+    {
+      id: "6",
+      name: "Ember",
+      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNG9-yTA20s2dFLEJamV7CilxHAIkU3Oc6HA&s"
+    },
+    {id: "7", name: "Storm", imgUrl: "https://i.ytimg.com/vi/X1oXuyG6YxI/maxresdefault.jpg"},
+  ],
+  messagesData: [
+    {id: "1", message: "Yo"},
+    {id: "2", message: "Its really your social network??"},
+    {id: "3", message: "Do you like gachi cinema??"},
+    {id: "4", message: "Yes."},
+  ]
 }
 
 export const dialogsReducer = (state: MessagesPageType = initialState, action: ActionsType) => {
-    switch (action.type) {
-        case 'ADD-MESSAGE':
-            const newMessage: MessageType = {id: "5", message: state.newMessageText}
-            return {
-                ...state,
-                messagesData: [...state.messagesData, newMessage],
-                newMessageText: ''
-            }
-        case 'UPDATE-NEW-MESSAGE-TEXT':
-            const newMessageText = action.newMessage
-            return {...state, newMessageText}
-    }
-    return state
+  switch (action.type) {
+    case 'ADD-MESSAGE':
+      const newMessage: MessageType = {id: "5", message: action.message}
+      return {
+        ...state,
+        messagesData: [...state.messagesData, newMessage]
+      }
+  }
+  return state
 }
 
-export const addMessageAC = (): AddMessageActionType => (
-    {type: 'ADD-MESSAGE'}
-)
-export const updateMessageTextAC = (text: string): UpdateMessageTextType => (
-    {type: 'UPDATE-NEW-MESSAGE-TEXT', newMessage: text}
+export const addMessageAC = (message: string): AddMessageActionType => (
+    {type: 'ADD-MESSAGE', message}
 )
--- src/components/profile/model/profile-reducer.ts
+++ src/components/profile/model/profile-reducer.ts
@@ -4,40 +4,9 @@ import {
   AddPostActionType,
   SetUserProfileActionType,
   SetUserStatusActionType,
-  UpdatePostTextActionType
 } from "../../../shared/types/action-types";
 import {profileAPI} from "../../../shared/api/api";
 
-// let initialState: ProfilePageType = {
-//   postsData: [
-//     {id: "1", message: "Yo", likes: 12},
-//     {id: "2", message: "Its my social network!!!", likes: 73},
-//     {id: "3", message: "I hope serega pirat will be my fan", likes: 999},
-//   ],
-//   profile: {
-//     aboutMe: 'Joskiy 4el',
-//     contacts: {
-//       facebook: null,
-//       website: 'pornhub.com',
-//       vk: 'Контора пидарасов',
-//       twitter: null,
-//       instagram: 'залупа.com',
-//       youtube: null,
-//       github: 'https://github.com/nedonebo21',
-//       mainLink: null
-//     },
-//     fullName: 'Stanislav',
-//     lookingForAJob: true,
-//     lookingForAJobDescription: 'yo',
-//     photos: {
-//       large: 'https://i.ytimg.com/vi/qbxteW4kI6k/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGHEgOyhyMA8=&rs=AOn4CLDiLrIP6bTF4-NrmKQH970OwjNRsg',
-//       small: 'https://i.ytimg.com/vi/qbxteW4kI6k/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGHEgOyhyMA8=&rs=AOn4CLDiLrIP6bTF4-NrmKQH970OwjNRsg',
-//     },
-//     userId: 32493,
-//   },
-//   newPostText: 'Yo',
-//   status: '',
-// }
 let initialState: ProfilePageType = {
   postsData: [
     {id: "1", message: "Yo", likes: 12},
@@ -65,21 +34,16 @@ let initialState: ProfilePageType = {
     },
     userId: 32493,
   },
-  newPostText: '',
   status: '',
 }
 export const profileReducer = (state: ProfilePageType = initialState, action: ActionsType) => {
   switch (action.type) {
     case 'ADD-POST':
-      const newPost: PostType = {id: "5", message: state.newPostText, likes: 0}
+      const newPost: PostType = {id: "5", message: action.newPost, likes: 0}
       return {
         ...state,
         postsData: [newPost, ...state.postsData],
-        newPostText: ''
       }
-    case 'UPDATE-NEW-POST-TEXT':
-      const newPostText = action.newText
-      return {...state, newPostText}
     case 'SET-USER-PROFILE':
       return {...state, profile: action.profile}
     case 'SET-STATUS':
@@ -88,11 +52,8 @@ export const profileReducer = (state: ProfilePageType = initialState, action: Ac
   return state
 }
 
-export const addPostAC = (): AddPostActionType => (
-    {type: 'ADD-POST'}
-)
-export const updatePostTextAC = (text: string): UpdatePostTextActionType => (
-    {type: 'UPDATE-NEW-POST-TEXT', newText: text}
+export const addPostAC = (newPost: string): AddPostActionType => (
+    {type: 'ADD-POST', newPost}
 )
 export const setUserProfile = (profile: ProfileType): SetUserProfileActionType => (
     {type: 'SET-USER-PROFILE', profile}
