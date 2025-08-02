47f35b06a11dea45533ba289e60060b1b95f8244
nedonebo21 <cursed.void10007@gmail.com>
2025-08-02 17:54:43 +0400

    refactor userId for auth user(79)

--- src/components/profile/profile-container.tsx
+++ src/components/profile/profile-container.tsx
@@ -17,13 +17,14 @@ type ProfileApiType = {
   isAuth: boolean
   getUserStatusThunk: (userId: number) => void
   updateStatusThunk: (status: string) => void
+  userId: number
   status: string
 } & RouteComponentProps<RouteParams>
 
 export class ProfileApiComponent extends React.Component<ProfileApiType> {
   componentDidMount() {
     let userId = parseInt(this.props.match.params.userId, 10);
-    if (!userId) userId = 32493
+    if (!userId) userId = this.props.userId
     this.props.getUserProfileThunk(userId)
     this.props.getUserStatusThunk(userId)
   }
@@ -35,7 +36,9 @@ export class ProfileApiComponent extends React.Component<ProfileApiType> {
 
 let mapStateToProps = (state: StateType) => ({
   profile: state.profilePage.profile,
-  status: state.profilePage.status
+  status: state.profilePage.status,
+  userId: state.auth.userId,
+  isAuth: state.auth.isAuth
 })
 
 const ComposedComponent = compose<React.ComponentType>(
