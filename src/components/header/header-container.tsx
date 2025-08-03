4e69dbd88280ecf01c53e7fbe12aa77754b51212
nedonebo21 <cursed.void10007@gmail.com>
2025-08-03 12:18:59 +0400

    refactor header-container comp(80)

--- src/components/header/header-container.tsx
+++ src/components/header/header-container.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 import {Header} from "./header";
-import axios from "axios";
 import {connect} from "react-redux";
-import {getAuthUserDataThunkCreator, logoutThunkCreator, setAuthUserDataAC} from "../../shared/model/auth-reducer";
+import {logoutThunkCreator} from "../../shared/model/auth-reducer";
 import {StateType} from "../../shared/types/state-types";
-import {authAPI} from "../../shared/api/api";
 
 type HeaderApiType = {
   login: string | null
   isAuth: boolean
   logout: () => void
-  getAuthUserDataThunk: () => void
 }
 
 export class HeaderAPIComponent extends React.Component<HeaderApiType> {
-  componentDidMount() {
-    this.props.getAuthUserDataThunk()
-  }
-
   render() {
     return <Header logout={this.props.logout} login={this.props.login} isAuth={this.props.isAuth}/>
   }
@@ -31,6 +24,5 @@ const mapStateToProps = (state: StateType) => (
 )
 
 export const HeaderContainer = connect(mapStateToProps, {
-  getAuthUserDataThunk: getAuthUserDataThunkCreator,
   logout: logoutThunkCreator
 })(HeaderAPIComponent)
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import './shared/styles/global.css';
 import {store} from "./app/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './app/App';
+import App from './app/App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
