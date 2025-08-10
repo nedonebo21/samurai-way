665b01aaf9bceba478756b31308e823a72770f50
nedonebo21 <cursed.void10007@gmail.com>
2025-08-10 11:12:32 +0400

    add reselect/createSelector(83)

--- src/components/users/model/users-selectors.ts
+++ src/components/users/model/users-selectors.ts
@@ -1,6 +1,9 @@
 import {StateType} from "../../../shared/types/state-types";
+import {createSelector} from "reselect";
 
-export const getUsers = (state: StateType) => state.usersPage.users
+const getUsersSelector = (state: StateType) => state.usersPage.users
+export const getUsers = createSelector(getUsersSelector, (users) => users.filter(el => true))
+
 export const getPageSize = (state: StateType) => state.usersPage.pageSize
 export const getTotalUsersCount = (state: StateType) => state.usersPage.totalUsersCount
 export const getCurrentPage = (state: StateType) => state.usersPage.currentPage
