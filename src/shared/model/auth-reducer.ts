d6b3743b0afcba1f3bfbce69433307eff5a1f00f
nedonebo21 <cursed.void10007@gmail.com>
2025-08-24 11:32:18 +0400

    refactor auth thunks to async/await(90)

--- src/shared/model/auth-reducer.ts
+++ src/shared/model/auth-reducer.ts
@@ -20,29 +20,29 @@ export const authReducer = (state: AuthType = initialState, action: ActionsType)
 export const setAuthUserDataAC = (userId: number | null, email: string | null, login: string | null, isAuth: boolean): SetAuthUserDataType => (
     {type: 'SET-USER-DATA', data: {userId, email, login, isAuth}}
 )
-export const getAuthUserDataThunkCreator = () => (dispatch: DispatchType) => {
-  return authAPI.me().then(res => {
-    if (res.data.resultCode === 0) {
-      let {id, login, email} = res.data.data
-      dispatch(setAuthUserDataAC(id, email, login, true))
-    }
-  })
+export const getAuthUserDataThunkCreator = () => async (dispatch: DispatchType) => {
+  let res = await authAPI.me()
+
+  if (res.data.resultCode === 0) {
+    let {id, login, email} = res.data.data
+    dispatch(setAuthUserDataAC(id, email, login, true))
+  }
 }
 
-export const loginThunkCreator = (email: string, password: string, rememberMe: boolean = false) => (dispatch: DispatchType) => {
-  authAPI.login(email, password, rememberMe).then((res) => {
-    if (res.data.resultCode === 0) {
-      dispatch(getAuthUserDataThunkCreator())
-    } else {
-      let message: string = res.data.messages.length > 0 ? res.data.messages[0] : 'Some Error'
-      dispatch(stopSubmit('login', {_error: message}))
-    }
-  })
+export const loginThunkCreator = (email: string, password: string, rememberMe: boolean = false) => async (dispatch: DispatchType) => {
+  let res = await authAPI.login(email, password, rememberMe)
+
+  if (res.data.resultCode === 0) {
+    dispatch(getAuthUserDataThunkCreator())
+  } else {
+    let message: string = res.data.messages.length > 0 ? res.data.messages[0] : 'Some Error'
+    dispatch(stopSubmit('login', {_error: message}))
+  }
 }
-export const logoutThunkCreator = () => (dispatch: DispatchType) => {
-  authAPI.logout().then((res) => {
-    if (res.data.resultCode === 0) {
-      dispatch(setAuthUserDataAC(null, null, null, false))
-    }
-  })
+export const logoutThunkCreator = () => async (dispatch: DispatchType) => {
+  let res = await authAPI.logout()
+
+  if (res.data.resultCode === 0) {
+    dispatch(setAuthUserDataAC(null, null, null, false))
+  }
 }
