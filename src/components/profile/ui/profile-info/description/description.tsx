e59735ec3aab244d620ee4e72c54d510708762e8
nedonebo21 <cursed.void10007@gmail.com>
2025-09-01 10:40:15 +0400

    add Contact comp for render contacts data (97)

--- src/components/profile/ui/profile-info/description/description.tsx
+++ src/components/profile/ui/profile-info/description/description.tsx
@@ -45,49 +45,26 @@ export const Description = (props: DescriptionType) => {
                         </div>
                     </div>
                     <div className={s.desc_items}><strong className={s.desc_title}>Contacts:</strong>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>FaceBook: </strong>
-                            {props.profile.contacts.facebook ?
-                                <a href={props.profile.contacts.facebook}>{props.profile.contacts.facebook}</a> : 'No Link'}
-                        </div>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>WebSite: </strong>
-                            {props.profile.contacts.website ?
-                                <a href={props.profile.contacts.website}>{props.profile.contacts.website}</a> : 'No Link'}
-                        </div>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>VK: </strong>
-                            {props.profile.contacts.vk ?
-                                <a href={props.profile.contacts.vk}>{props.profile.contacts.vk}</a> : 'No Link'}
-                        </div>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>Twitter: </strong>
-                            {props.profile.contacts.twitter ?
-                                <a href={props.profile.contacts.twitter}>{props.profile.contacts.twitter}</a> : 'No Link'}
-                        </div>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>Inst: </strong>
-                            {props.profile.contacts.instagram ?
-                                <a href={props.profile.contacts.instagram}>{props.profile.contacts.instagram}</a> : 'No Link'}
-                        </div>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>YT: </strong>
-                            {props.profile.contacts.youtube ?
-                                <a href={props.profile.contacts.youtube}>{props.profile.contacts.youtube}</a> : 'No Link'}
-                        </div>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>github: </strong>
-                            {props.profile.contacts.github ?
-                                <a href={props.profile.contacts.github}>{props.profile.contacts.github}</a> : 'No Link'}
-                        </div>
-                        <div className={s.desc_item}>
-                            <strong className={s.desc_sub_title}>MainLink: </strong>
-                            {props.profile.contacts.mainLink ?
-                                <a href={props.profile.contacts.mainLink}>{props.profile.contacts.mainLink}</a> : 'No Link'}
-                        </div>
+                        {Object.entries(props.profile.contacts).map(([key,value]) => {
+                            return <Contact key={key} contactTitle={key} contactValue={value}/>
+                        })}
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
+type ContactPropsType = {
+    contactTitle: string
+    contactValue: string | null
+}
+const Contact = (props: ContactPropsType) => {
+    return (
+        <div className={s.desc_item}>
+            <strong className={s.desc_sub_title}>{props.contactTitle}: </strong>
+            {props.contactValue ?
+                <a href={props.contactValue}>{props.contactValue}</a> : 'No Link'}
+        </div>
+    )
+}
